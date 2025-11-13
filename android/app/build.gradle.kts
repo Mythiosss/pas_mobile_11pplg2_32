fd0874d7265cf3ce546454cd68e80bfb9dbbc014
Mythiosss <mythiosmain@gmail.com>
2025-11-13 07:00:36 +0700

    fix error and add dependencies + run test (done)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.pas_mobile_11pplg2_32"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "29.0.14206865"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
